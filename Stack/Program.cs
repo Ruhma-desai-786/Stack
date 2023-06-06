608be1c8445597f117264d69990ba115398aeea7
Ruhma-desai-786 <romakesari2734@gmail.com>
2023-06-06 18:26:02 +0530

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,59 @@
+﻿using Stack;
+using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the Choice\n1)Stack\n2)Pop and Peek\n3)Enqueue\n4)Dequeue");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        StackEx ex = new StackEx();
+                        ex.Push(70);
+                        ex.Push(30);
+                        ex.Push(56);
+                        ex.Display();
+                        break;
+                    case 2:
+                        //StackEx ex = new StackEx();
+                        //ex.Push(70);
+                        //ex.Push(30);
+                        //ex.Push(56);
+                        //ex.Display();
+                        //ex.Pop();
+                        //ex.Pop();
+                        //ex.Pop();
+                        //ex.Display();
+                        //ex.Peek();
+                        //ex.Display();
+                        //break;
+                    case 3:
+                        QueueEx qx =new QueueEx();
+                        qx.Enqueue(56);
+                        qx.Enqueue(30);
+                        qx.Enqueue(70);
+                        qx.DisplayQueue();
+                        break;
+                    case 4:
+                        QueueEx qx = new QueueEx();
+                        qx.Enqueue(56);
+                        qx.Enqueue(30);
+                        qx.Enqueue(70);
+                        qx.DisplayQueue();
+                        qx.Dequeue();
+                        qx.DisplayQueue();
+                        break;
+                }
+               
+            
+            }
+        }
+    }
+}
---
+++ Stack/QueueEx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class QueueEx
+    {
+        Node front;
+        Node rear;
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear= rear.Next;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if(this.front==null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                while(this.front!=null)
+                {
+                    Console.WriteLine("{0} is the Deleted Element in the Queue--",this.front.Data);
+                    this.front = this.front.Next;
+                }
+            }
+        }
+        public void DisplayQueue()
+        {
+            
+            if(front==null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Node temp = front;
+                while(temp!=null)
+                {
+                    Console.WriteLine(" | "+temp.Data+" | ");
+                    temp=temp.Next;
+                }
+        }
+
+    }
+}
